d56ad9dc044f7de43875ed4ddfd71e5722856e16
Icelk <main@icelk.dev>
2022-03-13 12:30:18 +0100

    Added example.

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,31 @@
 //!
 //! For best results, please add [`ValueHint`]s to every argument that takes a value.
 //! This greatly improves the shell completion experience.
+//!
+//! # Examples
+//!
+//! ```rust
+//! // Create a command from the crate metadata
+//! let mut command = clap::command!();
+//! // Register `complete` subcommand
+//! command = clap_autocomplete::add_subcommand(command);
+//!
+//! // Add other arguments and subcommands
+//!
+//! let command_copy = command.clone();
+//! // Resolve the matches
+//! let matches = command.get_matches();
+//! if let Some(result) = clap_autocomplete::test_subcommand(&matches, command_copy) {
+//!     if let Err(err) = result {
+//!         eprintln!("Insufficient permissions: {err}");
+//!         std::process::exit(1);
+//!     } else {
+//!         std::process::exit(0);
+//!     }
+//! } else {
+//!     // Continue with the application logic
+//! }
+//! ```
 
 use std::fs;
 use std::fs::create_dir_all;
